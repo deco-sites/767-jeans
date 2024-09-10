dc9e1efcf7f965b8bfce5e3a15f323b13539ed86
@yuri_assuncx <killerkoll2012@gmail.com>
2024-09-10 16:28:18 -0300

    chore: bump versions

--- sections/Images/Banner.tsx
+++ sections/Images/Banner.tsx
@@ -4,7 +4,7 @@ import Section from "../../components/ui/Section.tsx";
 import { clx } from "../../sdk/clx.ts";
 
 export interface Props {
-  title: string;
+  title?: string;
   description?: HTMLWidget;
 
   images: {
@@ -12,59 +12,66 @@ export interface Props {
     desktop: ImageWidget;
   };
 
+  href: string;
   cta?: {
-    href: string;
-    label: string;
+    label?: string;
   };
 }
 
-function Banner({ title, description, images, cta }: Props) {
+function Banner({ title, description, images, href, cta }: Props) {
   return (
     <Section.Container>
       <div class="relative bg-base-200 mx-5 sm:mx-0">
-        <Picture>
-          <Source
-            media="(max-width: 640px)"
-            src={images.mobile}
-            width={335}
-            height={572}
-          />
-          <Source
-            media="(min-width: 640px)"
-            src={images.desktop}
-            width={1320}
-            height={480}
-          />
-          <img src={images.desktop} alt={title} class="w-full object-cover" />
-        </Picture>
-
-        <div
-          class={clx(
-            "absolute left-0 top-0",
-            "p-5 sm:p-10 md:py-20 md:px-[60px]",
-            "flex flex-col",
-            "h-full max-w-full sm:max-w-[33%] md:max-w-[50%] justify-center",
-          )}
-        >
-          {title && <span class="font-bold text-7xl text-primary">{title}
-          </span>}
-          {description && (
-            <span
-              class="font-normal text-sm md: pt-4 pb-12"
-              dangerouslySetInnerHTML={{ __html: description }}
+        <a href={href}>
+          <Picture>
+            <Source
+              media="(max-width: 640px)"
+              src={images.mobile}
+              width={335}
+              height={572}
             />
-          )}
-          <div class="">
-            {cta && (
-              <a
-                href={cta.href}
-                class="btn btn-primary no-animatio w-fit border-0 min-w-[180px]"
+            <Source
+              media="(min-width: 640px)"
+              src={images.desktop}
+              width={1320}
+              height={480}
+            />
+            <img src={images.desktop} alt={title} class="w-full object-cover" />
+          </Picture>
+        </a>
+
+        {title || cta?.label && (
+              <div
+                class={clx(
+                  "absolute left-0 top-0",
+                  "p-5 sm:p-10 md:py-20 md:px-[60px]",
+                  "flex flex-col",
+                  "h-full max-w-full sm:max-w-[33%] md:max-w-[50%] justify-center",
+                )}
               >
-                {cta.label}
-              </a>
+                {title && (
+                  <span class="font-bold text-7xl text-primary">{title}</span>
+                )}
+
+                {description && (
+                  <span
+                    class="font-normal text-sm md: pt-4 pb-12"
+                    dangerouslySetInnerHTML={{ __html: description }}
+                  />
+                )}
+
+                {cta && (
+                  <div>
+                    <a
+                      href={href}
+                      class="btn btn-primary no-animatio w-fit border-0 min-w-[180px]"
+                    >
+                      {cta.label}
+                    </a>
+                  </div>
+                )}
+              </div>
             )}
-          </div>
-        </div>
       </div>
     </Section.Container>
   );
